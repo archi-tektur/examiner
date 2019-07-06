41b8c47d21536627baab0b40e319ae1f04197086
Oskar 'archi_tektur' Barcz <kontakt@archi-tektur.pl>
2019-07-06 22:28:14 +0200

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+let Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+  .setOutputPath('public/build/')
+  .setPublicPath('/build')
+  .addEntry('app', './assets/ts/app.ts')
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .configureBabel(() => {}, {
+    useBuiltIns: 'usage',
+    corejs: 3,
+  })
+  .enableSassLoader()
+  .enableTypeScriptLoader()
+  .enableIntegrityHashes(Encore.isProduction());
+
+module.exports = Encore.getWebpackConfig();
