4917f8fa125d01752bda9faed9e87c2fe4e64777
Oskar 'archi_tektur' Barcz <kontakt@archi-tektur.pl>
2019-07-06 23:02:38 +0200

    Added pre-persist date setting and others

--- src/Entity/Users/AbstractUser.php
+++ src/Entity/Users/AbstractUser.php
@@ -2,12 +2,15 @@
 
 namespace App\Entity\Users;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Exception;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Representation of loggable user
  *
+ * @ORM\HasLifecycleCallbacks()
  * @package App\Entity\Users
  */
 abstract class AbstractUser implements UserInterface
@@ -49,89 +52,62 @@ abstract class AbstractUser implements UserInterface
     protected $surmame;
 
     /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
+
+    /**
      * User's e-mail
      * @ORM\Column(type="string", length=120, nullable=true)
      */
     protected $mail;
 
-    /**
-     * @return mixed
-     */
-    public function getUuid()
+    public function getUuid(): ?int
     {
         return $this->uuid;
     }
 
-    /**
-     * @param mixed $uuid
-     * @return self
-     */
-    public function setUuid($uuid): self
+    public function setUuid(int $uuid): self
     {
         $this->uuid = $uuid;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getName()
+    public function getName(): ?string
     {
         return $this->name;
     }
 
-    /**
-     * @param mixed $name
-     * @return self
-     */
     public function setName($name): self
     {
         $this->name = $name;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getSurmame()
+    public function getSurmame(): ?string
     {
         return $this->surmame;
     }
 
-    /**
-     * @param mixed $surmame
-     * @return self
-     */
-    public function setSurmame($surmame): self
+    public function setSurmame(string $surmame): self
     {
         $this->surmame = $surmame;
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getMail()
+    public function getMail(): ?string
     {
         return $this->mail;
     }
 
-    /**
-     * @param mixed $mail
-     * @return self
-     */
-    public function setMail($mail): self
+    public function setMail(string $mail): self
     {
         $this->mail = $mail;
         return $this;
     }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUsername(): string
+    /** @see UserInterface */
+    public function getUsername(): ?string
     {
         return (string)$this->username;
     }
@@ -142,9 +118,18 @@ abstract class AbstractUser implements UserInterface
         return $this;
     }
 
-    /**
-     * @see UserInterface
-     */
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /** @see UserInterface */
     public function getRoles(): array
     {
         $roles = $this->roles;
@@ -159,9 +144,7 @@ abstract class AbstractUser implements UserInterface
         return $this;
     }
 
-    /**
-     * @see UserInterface
-     */
+    /** @see UserInterface */
     public function getPassword(): string
     {
         return (string)$this->password;
@@ -173,17 +156,36 @@ abstract class AbstractUser implements UserInterface
         return $this;
     }
 
-    /**
-     * @see UserInterface
-     */
+    /** @see UserInterface */
     public function getSalt()
     {
     }
 
-    /**
-     * @see UserInterface
-     */
+    /** @see UserInterface */
     public function eraseCredentials(): void
     {
+    }
+
+    /**
+     * Auto-generate 6-digit UUID
+     * @ORM\PrePersist()
+     *
+     * @throws Exception actually never
+     */
+    public function generateUuid(): void
+    {
+        $this->uuid = random_int(100000, 999999);
+    }
+
+    /**
+     * Auto-add creation date
+     * @ORM\PrePersist()
+     *
+     * @param DateTime $dateTime
+     * @throws Exception actually never
+     */
+    public function setCreation(DateTime $dateTime): void
+    {
+        $this->createdAt = $dateTime;
     }
 }
